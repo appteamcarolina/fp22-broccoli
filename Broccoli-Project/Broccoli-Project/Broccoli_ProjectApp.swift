89da544cd22e07f140f3f56cb1ba185a8e5cae02
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-03-29 17:32:32 -0400

    added project folder and Xcode skeleton

---
+++ Broccoli-Project/Broccoli-Project/Broccoli_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Broccoli_ProjectApp.swift
+//  Broccoli-Project
+//
+//  Created by AlecNipp on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Broccoli_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
