00f69d4740d9d1b89e39ec948cd1730bf894688c
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-03-31 17:34:20 -0400

    created models for TasteDive API, and getDummyData method

--- Broccoli-Project/Broccoli-Project/TasteDiveService.swift
+++ Broccoli-Project/Broccoli-Project/TasteDiveService.swift
@@ -8,5 +8,44 @@
 import Foundation
 
 public final class TasteDiveService {
-    static func getDummyData() -> []
+    
 }
+
+extension TasteDiveService {
+    static func getExampleData() -> [TDResult] {
+        var dummy: [TDResult] = []
+        for i in 0...10 {
+            dummy.append(TDResult(name: "Artist \(i)", type: "music"))
+        }
+        return dummy
+    }
+}
+
+struct TDResponse: Decodable {
+    let data: TDSimilarData
+    
+    enum CodingKeys: String, CodingKey {
+        case data = "Similar"
+    }
+}
+
+struct TDSimilarData: Decodable {
+    
+    let results: [TDResult]
+    enum CodingKeys: String, CodingKey {
+        case results = "Results"
+    }
+    
+}
+
+
+struct TDResult: Codable {
+    let name: String
+    let type: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "Name"
+        case type = "Type"
+    }
+}
+
