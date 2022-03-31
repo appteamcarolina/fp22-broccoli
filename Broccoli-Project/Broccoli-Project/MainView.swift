71fc0c4bd0545860708381535af2166e9e0343cf
Peter Wi <peter.h.wi96@gmail.com>
2022-03-31 18:01:20 -0400

    Update MainView with basic UI

--- Broccoli-Project/Broccoli-Project/MainView.swift
+++ Broccoli-Project/Broccoli-Project/MainView.swift
@@ -7,10 +7,61 @@
 
 import SwiftUI
 
+enum MediaType: String, CaseIterable {
+    case music = "music"
+    case movie = "movie"
+    case show = "show"
+    case book = "book"
+    case author = "author"
+}
+
 struct MainView: View {
+    private let mediaTypes = MediaType.allCases
+    @State private var inputKeywords = [String]()
+    @State private var inputTypes = [Int]()
+    @State private var searchString = ""
+    @State private var selectedType = 0
+    
     var body: some View {
-        Text("Hello, world!")
+        ScrollView(.vertical, showsIndicators: false) {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                TextField("Search Bar", text: $searchString)
+                    .disableAutocorrection(true)
+                    .onSubmit {
+                        inputKeywords.append(searchString)
+                        searchString = ""
+                        inputTypes.append(selectedType)
+                        selectedType = 0
+                    }
+            }
             .padding()
+            .overlay(RoundedRectangle(cornerRadius: 30)
+                .stroke())
+            .padding()
+            Picker("Choose a type", selection: $selectedType) {
+                ForEach(MediaType.allCases, id: \.self) {
+                    Text($0.rawValue)
+                }
+            }
+            .pickerStyle(.segmented)
+            .padding()
+            
+            Button(action: {
+                
+            }) {
+                Text("Recommend Artists")
+                    .padding()
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+            }
+            
+            ForEach(inputKeywords, id: \.self) { keyword in
+                Text(keyword)
+            }
+        }
+        
     }
 }
 
