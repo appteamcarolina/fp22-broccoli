f27714100bc6d85f0363ba5fd046cfe01cdd11a3
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-03-31 17:54:43 -0400

    basic skeleton of ResultsListView and ArtistView

--- Broccoli-Project/Broccoli-Project/ArtistEntryView.swift
+++ Broccoli-Project/Broccoli-Project/ArtistEntryView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct ArtistEntryView: View {
+    let artist: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink(destination: ArtistDetailedView()) {
+            Text(artist)
+            .bold()
+        }
     }
 }
 
 struct ArtistEntryView_Previews: PreviewProvider {
     static var previews: some View {
-        ArtistEntryView()
+        ArtistEntryView(artist: "Pink Floyd")
     }
 }
--- Broccoli-Project/Broccoli-Project/ResultsListView.swift
+++ Broccoli-Project/Broccoli-Project/ResultsListView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct ResultsListView: View {
+    @ObservedObject var vm: MainViewModel = MainViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(TasteDiveService.getExampleData(), id: \.name) { item in
+                ArtistEntryView(artist: item.name)
+            }
+        }
     }
 }
 
