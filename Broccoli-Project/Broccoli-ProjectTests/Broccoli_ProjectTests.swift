47bce6009c3ba21538eb1ba163fff52d952fb10a
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-04-18 15:34:33 -0400

    fixed tests. set up for more

--- Broccoli-Project/Broccoli-ProjectTests/Broccoli_ProjectTests.swift
+++ Broccoli-Project/Broccoli-ProjectTests/Broccoli_ProjectTests.swift
@@ -9,24 +9,27 @@ import XCTest
 @testable import Broccoli_Project
 
 class Broccoli_ProjectTests: XCTestCase {
-    var exampleQuery: [TDItem]!
+    var exampleMusicQuery: [TDQuery]!
+    var exampleMovieQuery: [TDQuery]!
     
     override func setUpWithError() throws {
-        exampleQuery = [TDItem(name: "red hot chili peppers", type: "band")]
+        exampleMusicQuery = [TDQuery(name: "red hot chili peppers", type: MediaType.music)]
+        exampleMovieQuery = [TDQuery(name: "Rush", type: MediaType.movie)]
+
     }
     
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func testExample() async {
+    func testMusicFetch() async {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
         do {
-            let result = try await TasteDiveService.fetchRecommendations(queryItems: exampleQuery)
+            let result = try await TasteDiveService.fetchRecommendations(queryItems: exampleMusicQuery)
             print(result)
             XCTAssertEqual(result.last!.name, "The Beatles")
         }
@@ -36,11 +39,5 @@ class Broccoli_ProjectTests: XCTestCase {
         
     }
     
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
