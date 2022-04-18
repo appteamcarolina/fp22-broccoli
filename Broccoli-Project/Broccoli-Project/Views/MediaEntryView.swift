2e7d9a14f08825cab7b44aecbc0bd65768247d11
Peter Wi <peter.h.wi96@gmail.com>
2022-04-18 16:58:51 -0400

    fetchURL only when the url is nil.

--- Broccoli-Project/Broccoli-Project/Views/MediaEntryView.swift
+++ Broccoli-Project/Broccoli-Project/Views/MediaEntryView.swift
@@ -19,11 +19,12 @@ struct MediaEntryView: View {
             }
         }
         .task {
-            DispatchQueue.global().async {
-                vm.fetchImageURL()
+            if (vm.imgURLString == nil) {
+                DispatchQueue.global().async {
+                    vm.fetchImageURL()
+                }
             }
         }
-        
     }
 }
 
